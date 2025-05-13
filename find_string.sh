370969912019901b409f04dc43ef168e3de74118
Mahamadou Amine <mahamadouamine@iMac-de-Souleymane.local>
2025-05-14 00:41:08 +0100

    azed

--- find_string.sh
+++ find_string.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+read ficher
+
+read texte_rechercher
+
+num_occurence=$(grep "$texte_rechercher" "$ficher")
+
+if [ "$num_occurence" ]; then
+echo "La chaîne "$texte_rechercher" existe dans "$ficher"."
+else 
+echo "La chaine "$texte_rechercher" n'existe pas dans "$ficher"."
+fi
