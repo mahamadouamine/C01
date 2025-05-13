d068e4dd58bfcddc09f5779fb3878b811f7677fa
Mahamadou Amine <mahamadouamine@iMac-de-Souleymane.local>
2025-05-14 00:27:00 +0100

    azerty

--- cli_arguments.sh
+++ cli_arguments.sh
@@ -1,7 +1,1 @@
 #!/bin/bash
-arg="hello world"
-i=1
-    echo "hello world"
-for arg in "$@"; do
-    echo "argument $((i++)) : $arg"
-done
--- find_string.sh
+++ find_string.sh
@@ -1,13 +1,1 @@
 #!/bin/bash
-
-ficher="file.txt"
-
-texte_rechercher="bonjour"
-
-num_occurence=$(grep "$texte_rechercher" "$ficher")
-
-if [ "$num_occurence" -gt 0 ]; then
-echo "La chaîne "$texte_rechercher" existe dans "$ficher"."
-else 
-echo "La chaine "$texte_rechercher" n'existe pas dans "$ficher"."
-fi
