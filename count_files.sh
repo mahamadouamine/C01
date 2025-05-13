c94af65e53bb53499168a004f7f4e2b3561b37cf
Mahamadou Amine <mahamadouamine@iMac-de-Souleymane.local>
2025-05-13 21:45:31 +0100

    commit

--- count_files.sh
+++ count_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #ce script nous permet de compter le nombre de fichiers dans un repertore
 read myfolder
-count=$(ls -1 $myfolder | wc -l)
+count=$(ls $myfolder | wc -l)
 counts=$(echo $count)
 echo "Le répertoire "$myfolder" contient "$counts" fichier(s)."
