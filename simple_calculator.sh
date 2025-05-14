e4b00046b4e1dd020c7cc1b0083fdddbb2655790
Mahamadou Amine <mahamadouamine@iMac-de-Souleymane.local>
2025-05-14 03:09:02 +0100

    committ

---
+++ simple_calculator.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+a=$1
+b=$2
+op=$3
+
+read a b op
+
+if [ "$op" = "+" ]; then
+    Resultat=$(($a + $b))
+
+elif [ "$op" = "-" ]; then
+     Resultat=$(($a - $b))
+
+elif [ "$op" = "*" ]; then
+     Resultat=$(($a * $b))
+
+elif [ "$op" = "/" ]; then
+     Resultat=$(($a / $b))   
+fi
+echo "Resultat= $Resultat" 
