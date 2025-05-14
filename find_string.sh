76cb06655b39e4c4c2b0cb4d73305040a32b13b2
Mahamadou Amine <mahamadouamine@iMac-de-Souleymane.local>
2025-05-14 01:43:52 +0100

    honte a sorko

--- find_string.sh
+++ find_string.sh
@@ -5,7 +5,7 @@ read f
 read c
 
 if grep -q "$c" "$f"; then
-    echo "La chaîne '$c' a été trouvée dans '$f'."
+    echo "La chaîne '$c' a été trouvée dans $f."
 else
-    echo "La chaîne '$c' n'a pas été trouvée dans '$f'."
+    echo "La chaîne '$c' n'a pas été trouvée dans $f."
 fi
